6b438a6144b164ec3210669a2cde0c2e9ac08441
Samantha Lincroft <petraarkian.heg.gov@gmail.com>
2018-01-09 11:56:23 -0500

    setup routing through index.js

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var app = express();
 var chalk = require('chalk');
+var volleyball = require('volleyball');
+var bodyparser = require('body-parser');
+var index = require('./routes/index.js');
 // var routes = require('./routes'); uncomment this when you are ready to connect your router
+
+app.use('/', volleyball);
+app.use('/', bodyparser.json());
+
+
+app.use('/', index);
 
 app.get('/', function (req, res, next) {
   res.send('Root Route');
 });
-
 
 var PORT = 8000;
 
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,10 @@
 // Where you will connect your other routers
+var express = require('express');
+var router = express.Router();
+
+router.use(function (req, res, next) {
+  console.log('using routing');
+  next();
+});
+
+module.exports = router;
